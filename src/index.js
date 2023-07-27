51c77c9c85141d0ceb55c41caaf77ede513a6e9a
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-27 10:44:24 +0300

    CompletedFetching

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
   
+const form = document.querySelector('form')
+form.addEventListener('submit',(event) => {
+event.preventDefault()
+
+//Accessing the value directly from the DOM element
+const val = document.querySelector('input#searchByID')
+console.log(val.value)
+
+fetch(`http://localhost:3000/movies/${val.value}`)
+.then(resp => resp.json())
+.then(data => {
+    const title = document.querySelector('section#movieDetails h4')
+    const summary = document.querySelector('section#movieDetails p')
+
+    title.innerText = data.title
+    summary.innerText = data.summary
+})
+})
 }
 
 document.addEventListener('DOMContentLoaded', init);
